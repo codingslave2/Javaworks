7ca92b77aeacfda2f4250027a3dc0145909d316a
codingslave2 <codingslavekim121@gmail.com>
2023-05-15 15:18:45 +0900

    first commit

---
+++ ch01/src/hello/Hellojava.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		//println() 줄바꿈되며 출력
+//		여러줄 주석
+//		자료형 - 숫자, 문자, 불리언, 배열
+		
+		System.out.println("Hello~ Java!");
+		System.out.println(2.54);
+		System.out.println(10 > -10); //불리언(boolean) - true
+		System.out.println(10+11);
+		System.out.println(10 + '1'); //한 문자는 홀따옴표 사용, 여러 문자는 쌍따옴표 사용, 아스키 코드 1 ~ 49, 10+49 = 59
+		System.out.println(10 + "1"); // 큰 자료형쪽으로 형변환
+		
+		
+	} //main() 닫기
+
+} //class 닫기 - 클래스 이름은 첫글자를 대문자로 씀
